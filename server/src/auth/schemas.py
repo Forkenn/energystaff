b3941bce31b9c709d26c32d47a2627ee6757360c
forkenn <103817264+Forkenn@users.noreply.github.com>
2025-03-27 17:33:51 +0300

    Add roles setup to users schema

--- server/src/auth/schemas.py
+++ server/src/auth/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import EmailStr
+from pydantic import EmailStr, model_validator
 from fastapi_users import schemas
 
 class SUserRead(schemas.BaseUser[int]):
@@ -9,6 +9,16 @@ class SUserCreate(schemas.CreateUpdateDictModel):
     name: str
     email: EmailStr
     password: str
+    is_edu: bool
+    is_employer: bool
+    is_applicant: bool
+
+    @model_validator(mode="after")
+    def check_roles(self):
+        if sum((self.is_edu, self.is_applicant, self.is_employer)) != 1:
+            raise ValueError("User can only have one role")
+        return self
+
 
 class SUserUpdate(schemas.BaseUserUpdate):
     pass
