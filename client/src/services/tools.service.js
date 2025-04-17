1d95d6662ea8ff352e08ea95e1e9f1104f922209
forkenn <103817264+Forkenn@users.noreply.github.com>
2025-04-17 21:35:33 +0300

    Add tools service methods

--- client/src/services/tools.service.js
+++ client/src/services/tools.service.js
@@ -24,6 +24,57 @@ class ToolsService {
         }
       })
   }
+  async getEduInstitutionbyId(id) {
+    return await instance
+      .get(`/tools/edu-institutions/${id}`)
+      .then((response) => response)
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response.data)
+          console.log(err.response.status)
+        } else if (err.request) {
+          console.log(err.request)
+        } else {
+          console.log('Error', err.message)
+        }
+      })
+  }
+  async getEduLevels() {
+    return await instance
+      .get(`/tools/edu-levels`)
+      .then((response) => response)
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response.data)
+          console.log(err.response.status)
+        } else if (err.request) {
+          console.log(err.request)
+        } else {
+          console.log('Error', err.message)
+        }
+      })
+  }
+  async getLocations(data) {
+    return await instance
+      .get(`/tools/locations`, {
+        params: {
+            q: data.q,
+            start: data.start,
+            end: data.end 
+        },
+      })
+      .then((response) => response)
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response.data)
+          console.log(err.response.status)
+        } else if (err.request) {
+          console.log(err.request)
+        } else {
+          console.log('Error', err.message)
+        }
+      })
+  }
 }
   
 export default new ToolsService()
